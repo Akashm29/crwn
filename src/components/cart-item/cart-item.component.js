1a14d5c73999e0682604cf78a15412385f6bd65d
akashm29 <akashmishra291997@gmail.com>
2020-06-22 23:36:04 +0530

    Add Add to cart Component in Ecommerce website

--- src/components/cart-item/cart-item.component.js
+++ src/components/cart-item/cart-item.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./cart-item.styles.scss";
-const cartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
     <div className="item-details">
@@ -11,3 +11,5 @@ const cartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     </div>
   </div>
 );
+
+export default CartItem;
